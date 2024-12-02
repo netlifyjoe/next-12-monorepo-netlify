1b0eea887046ae82ac51234d573b9dda725b8c2f
netlifyjoe <joe.keyes@netlify.com>
2024-12-02 17:29:47 -0500

    Redirect Test

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	assetPrefix: "https://next-12-monorepo-docs-netlify.netlify.app",
+	// assetPrefix: "https://next-12-monorepo-docs-netlify.netlify.app",
 	reactStrictMode: true,
 	// rewrites: async () => ({
 	// 	beforeFiles: [
