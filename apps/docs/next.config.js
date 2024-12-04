8230ecfb22d359260438b32b47cc26876b0db1a4
Anil Bridgpal <bridgpal@gmail.com>
2024-12-04 15:02:22 -0500

    reverting

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,19 +1,16 @@
 /** @type {import('next').NextConfig} */
+// testing something
 const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
-	assetPrefix: process.env.NODE_ENV === 'production' 
-	  ? '/'  // In production, serve from root
-	  : '/docs',  // In development, serve from /docs
+	assetPrefix: "/docs",
 	async rewrites() {
-	  // Only apply rewrites in development
-	  if (process.env.NODE_ENV !== 'production') {
-		return [{
-		  source: "/docs/_next/:path*",
-		  destination: "/_next/:path*",
-		}]
-	  }
-	  return []
-	}
-  };
-  module.exports = nextConfig;
+		return [
+			{
+				source: "/docs/_next/:path*",
+				destination: "/_next/:path*",
+			},
+		];
+	},
+};
+module.exports = nextConfig;
