af86a82de9eb2eaa1d7bca76ee211b1c725234b3
Anil Bridgpal <bridgpal@gmail.com>
2024-12-05 16:14:52 -0500

    new rewrite

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -4,13 +4,13 @@ const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
 	assetPrefix: "/docs",
-	async rewrites() {
-		return [
-			{
-				source: "/docs/_next/:path*",
-				destination: "/_next/:path*",
-			},
-		];
-	},
+	// async rewrites() {
+	// 	return [
+	// 		{
+	// 			source: "/docs/_next/:path*",
+	// 			destination: "/_next/:path*",
+	// 		},
+	// 	];
+	// },
 };
 module.exports = nextConfig;
